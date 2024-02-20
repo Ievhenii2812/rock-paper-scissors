a59f2071acf3459db3dd60545790a8522789587e
ievheniim <evmir1979@gmail.com>
2024-02-21 00:10:45 +0200

    added simple game logic

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GameController::class,'index'])->name('game.index');
+Route::post('/play', [GameController::class,'play'])->name('game.play');
